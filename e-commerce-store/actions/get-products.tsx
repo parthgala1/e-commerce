0ef870a6ddec08a8ce95a96c4f8db4977e86539f
parthpsg <66634291+parthgala1@users.noreply.github.com>
2024-07-06 15:01:44 +0530

    Added and completed Featured Page of the Store

---
+++ e-commerce-store/actions/get-products.tsx
@@ -0,0 +1,28 @@
+import { Product } from "@/types";
+import qs from "query-string";
+
+interface Query {
+  categoryId?: string;
+  sizeId?: string;
+  colorId?: string;
+  isFeatured?: boolean;
+}
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+const getProducts = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      colorId: query.colorId,
+      sizeId: query.sizeId,
+      categoryId: query.categoryId,
+      isFeatured: query.isFeatured,
+    },
+  });
+  const response = await fetch(URL);
+
+  return response.json();
+};
+
+export default getProducts;
--- e-commerce-store/app/(routes)/page.tsx
+++ e-commerce-store/app/(routes)/page.tsx
@@ -1,9 +1,25 @@
-const HomePage = () => {
-    return (
-        <div>
-            HomePage
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboards from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/ui/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const product = await getProducts({ isFeatured: true });
+  const billboard = await getBillboards("0cf6a81f-41cc-4f84-b970-c427134d7087");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 ">
+          <ProductList title="Featured Products" items={[]} />
         </div>
-    )
-}
+      </div>
+    </Container>
+  );
+};
 
 export default HomePage;
